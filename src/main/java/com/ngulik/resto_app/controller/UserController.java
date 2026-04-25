ad2ed24cbe01c9da2335b7e25950ca0a70df22e8
syarifulanam <em.syariful.anam@gmail.com>
2026-04-25 16:56:24 +0700

    get all users dengan repo langsung

--- src/main/java/com/ngulik/resto_app/controller/UserController.java
+++ src/main/java/com/ngulik/resto_app/controller/UserController.java
@@ -1,22 +1,26 @@
 package com.ngulik.resto_app.controller;
 
-import com.ngulik.resto_app.service.UserService;
+import com.ngulik.resto_app.entity.User;
+import com.ngulik.resto_app.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+    private final UserRepository userRepository;
 
     @GetMapping
-    public ResponseEntity<String> getAllUsers() {
-        return ResponseEntity.ok().body("OK");
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
     }
+
 
 }
